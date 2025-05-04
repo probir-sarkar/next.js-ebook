fa75a1391c3b14e281bfa083a88c4794312863ad
Probir Sarkar <probirsarkar855@gmail.com>
2025-05-04 11:19:33 +0530

    workflow added

--- cloudflare-api/src/db/schema.ts
+++ cloudflare-api/src/db/schema.ts
@@ -1,8 +1,9 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer, } from 'drizzle-orm/sqlite-core';
 
 export const subscribe = sqliteTable('subscribe', {
     id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
     email: text('email').notNull().unique(),
     source: text('source').notNull().default('website'),
     preferredFormat: text('preferred_format').notNull().default('pdf'),
+    emailSent: integer('email_sent', { mode: "boolean" }).notNull().default(false),
 });
--- cloudflare-api/src/index.ts
+++ cloudflare-api/src/index.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import { getDb } from '@/db'
 import { subscribe } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+export { NextJSEbookWorkflow } from './workflows'
 
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
@@ -23,7 +25,17 @@ const subscribeSchema = z.object({
 app.post('/subscribe', zValidator('json', subscribeSchema), async (c) => {
   const { email } = c.req.valid('json')
   const db = getDb(c.env)
+  // is email exists?
+  const existingEmail = await db.select().from(subscribe).where(eq(subscribe.email, email)).limit(1)
+  if (existingEmail.length > 0) {
+    return c.json({ message: 'Email already exists' })
+  }
   const result = await db.insert(subscribe).values({ email }).returning()
+  // const workflow = c.env.WORKFLOW.create({
+  //   params: {
+  //     email
+  //   }
+  // })
   return c.json({ result })
 })
 
