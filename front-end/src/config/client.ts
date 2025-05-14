0e6f8b91723e9e5a2e30bac8d6a8f681ceeadffb
Probir Sarkar <probirsarkar855@gmail.com>
2025-05-14 22:41:33 +0530

    env update

--- front-end/src/config/client.ts
+++ front-end/src/config/client.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import type { AppType } from "../../../cloudflare-api/src";
 
-export const client = hc<AppType>(process.env.CLOUDFLARE_API_URL!);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_CLOUDFLARE_API_URL!);
